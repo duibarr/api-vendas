ba36cc0edf56029107d6deda7b87f9e008e2a382
Eduardo de Paula <eduardoibarr56@gmail.com>
2022-12-21 15:19:37 -0300

    starting with unit tests

---
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -0,0 +1,9 @@
+describe('CreateCustomer', () => {
+    it('should be able to create a new customer', () => {
+        expect(5 + 5).toBe(10);
+    });
+
+    it('should not be able to create a new customer if the email already exists', () => {
+        expect(5 + 1).toBe(10);
+    });
+});
